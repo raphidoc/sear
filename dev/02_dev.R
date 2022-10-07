c6081d27994090e1a6eacec2f1e4e0df2a315d56
raphael <raphael.mabit@gmail.com>
2022-10-07 18:52:36 -0400

    add codecov token not the best way ...

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -50,7 +50,7 @@ devtools::build_vignettes()
 
 ## Code Coverage----
 ## Set the code coverage service ("codecov" or "coveralls")
-usethis::use_coverage()
+usethis::use_coverage(type = c("codecov"))
 
 # Create a summary readme for the testthat subdirectory
 covrpage::covrpage()
